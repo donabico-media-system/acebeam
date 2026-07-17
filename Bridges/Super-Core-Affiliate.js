99d3939b2811225287cc5b21e3342657ad2256fb
donabico-global-media <donabico-global-media@users.noreply.github.com>
2026-07-17 03:32:07 +0000

    deploy: 12bc1db4c1d9e0dc25840991832168f715b8484a

--- Bridges/Super-Core-Affiliate.js
+++ Bridges/Super-Core-Affiliate.js
@@ -4,7 +4,7 @@
  * [BIGTECH INTEGRATION: GOOGLEBOT / BINGBOT / MICROSOFT ADTECH SYNC]
  */
 (function() {
-    const ESEB_TIMESTAMP = 1784236394;
+    const ESEB_TIMESTAMP = 1784259125;
     const CONFIG = {
         brandId: "acebeam",
         targetLink: "#",
