c24ecdf4f7a4f82009cffedbc2dda5985fba42d6
donabico-global-media <donabico-global-media@users.noreply.github.com>
2026-07-24 14:06:10 +0000

    deploy: f96bda6e1dad12b41d2a3466574514778068df46

--- Bridges/Super-Core-Affiliate.js
+++ Bridges/Super-Core-Affiliate.js
@@ -3,7 +3,7 @@
  * [V-STAMP 24 AUTHENTICATED] | ID: acebeam
  */
 (function() {
-    const ESEB_TIMESTAMP = 1784888831;
+    const ESEB_TIMESTAMP = 1784901969;
     const CONFIG = {
         brandId: "acebeam",
         targetLink: "#INSERT_YOUR_AFFILIATE_LINK_HERE",
